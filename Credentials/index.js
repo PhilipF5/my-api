e71749f05b7f1cfbc7e45d3d24ffbc6d5e7ec068
Philip Fulgham <philfulgham@gmail.com>
2019-01-24 22:23:26 -0500

    Add query params to get latest Credential or latest Job

--- Credentials/index.js
+++ Credentials/index.js
@@ -2,11 +2,15 @@ const mongodb = require("mongodb");
 
 module.exports = async function(context, req) {
 	let client = await mongodb.connect(process.env.MONGODB_URI);
-	let documents = await client
-		.db("philipfulgham")
-		.collection("credentials")
-		.find()
-		.toArray();
+	let collection = client.db("philipfulgham").collection("credentials");
+
+	let documents = req.query.latest
+		? (await collection
+				.find()
+				.sort("start", -1)
+				.limit(1)
+				.toArray())[0]
+		: await collection.find().toArray();
 
 	context.res = {
 		body: documents,
