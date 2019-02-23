1d9b7431dfa6bb74ac1ef78b687f69978601e65e
Philip Fulgham <philfulgham@gmail.com>
2019-02-23 13:52:35 -0500

    Enhance Projects API with images and icons

--- Projects/index.js
+++ Projects/index.js
@@ -1,6 +1,7 @@
 const mongodb = require("mongodb");
 
 module.exports = async function(context, req) {
+	const STORAGE_ROOT = process.env.STORAGE_ROOT;
 	let client = await mongodb.connect(process.env.MONGODB_URI);
 	let db = await client.db("philipfulgham");
 
@@ -12,6 +13,29 @@ module.exports = async function(context, req) {
 			{ $lookup: { from: "skills", localField: "platform", foreignField: "name", as: "platform" } },
 			{ $unwind: { path: "$platform" } },
 			{ $unwind: { path: "$repo" } },
+			{
+				$addFields: {
+					image: { $concat: [STORAGE_ROOT, "projects/", { $toString: "$_id" }, ".png"] },
+					"platform.image": {
+						$concat: [STORAGE_ROOT, "skills/", { $toString: "$platform._id" }, ".svg"],
+					},
+					skills: {
+						$map: {
+							input: "$skills",
+							in: {
+								$mergeObjects: [
+									"$$this",
+									{
+										icon: {
+											$concat: [STORAGE_ROOT, "skills/", { $toString: "$$this._id" }, ".svg"],
+										},
+									},
+								],
+							},
+						},
+					},
+				},
+			},
 			{ $sort: { featured: -1, name: 1 } },
 		])
 		.toArray();
