7f2e04ce6f24b9cd43253e7021960ef610a1c6b5
Philip Fulgham <philfulgham@gmail.com>
2019-03-07 22:38:44 -0500

    Add icon field to Skills API

--- Skills/index.js
+++ Skills/index.js
@@ -1,11 +1,14 @@
 const mongodb = require("mongodb");
 
 module.exports = async function(context, req) {
+	const STORAGE_ROOT = process.env.STORAGE_ROOT;
 	let client = await mongodb.connect(process.env.MONGODB_URI);
 	let documents = await client
 		.db("philipfulgham")
 		.collection("skills")
-		.find()
+		.aggregate([
+			{ $addFields: { icon: { $concat: [STORAGE_ROOT, "skills/", { $toString: "$_id" }, ".svg"] } } }
+		])
 		.toArray();
 
 	context.res = {
